a45eb22257b7ccdb1a83bacf72f7cc7404931db6
John Harakas <john.harakas@protonmail.com>
2018-11-15 01:05:53 -0500

    added method to return json results

--- video_recommend.py
+++ video_recommend.py
@@ -1,12 +1,11 @@
 import pandas as pd
-import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-import re
 
 stop_words = set(stopwords.words('english'))
+
 clips = pd.read_csv('similar-staff-picks/similar-staff-picks-challenge-clips.csv')
 clip_categories = pd.read_csv('similar-staff-picks/similar-staff-picks-challenge-clip-categories.csv')
 categories = pd.read_csv('similar-staff-picks/similar-staff-picks-challenge-categories.csv')
@@ -15,13 +14,14 @@ clip_categories['cat'] = [[int(y) for y in x.split(', ')] for x in clip_categori
 category_map = {x: y for x,y in zip(categories['category_id'],categories['name'])}
 category_map_inv = {y: x for x,y in zip(categories['category_id'],categories['name'])}
 
-test_keys = [14434107, 249393804, 71964690, 78106175, 228236677, 11374425, 93951774, 35616659, 112360862, 116368488]
 
+# Helper functions
 
+# Map a category name to a category id
 def get_cat_name(n, category_map=category_map):
     return ' '.join([category_map[m].lower().replace(' ', '') for m in n])
 
-
+# Map a category id to a category name
 def get_cat_id(n, category_map=category_map):
     return [category_map_inv[m] for m in n]
 
@@ -29,7 +29,7 @@ def get_cat_id(n, category_map=category_map):
 def get_clip(cid, dataframe):
     return dataframe.loc[cid]
 
-
+# Remove stop-words from text
 def remove_stop_words(text):
     return ' '.join([w for w in word_tokenize(text) if not w in stop_words])
 
@@ -70,16 +70,19 @@ def preprocess(df):
 
 
 def get_word_vector(df):
+    # Corpus is coming from the "words" of each clip - { title, categories, caption }
     corpus = df['words'].tolist()
 
     cv = CountVectorizer(max_df=0.95)
     word_vector = cv.fit_transform(corpus)
 
+    # Create a TF-IDF document matrix
     tfidf = TfidfTransformer(smooth_idf=True, use_idf=True)
     tfidf.fit(word_vector)
 
     feature_names = cv.get_feature_names()
 
+    # Generate a list of keywords from the corpus for each clip
     df['keywords'] = ''
     for idx, row in df.iterrows():
         vector = tfidf.transform(cv.transform([row['words']]))
@@ -101,8 +104,6 @@ def recommend(clip_id, df, matrix):
     score_series = pd.Series(matrix[idx]).sort_values(ascending=False)
     top_10 = score_series.iloc[1:11]
 
-    recs = pd.DataFrame([])
-
     for i in top_10.index:
         recommended.append(list(df.index)[i])
     print(top_10)
@@ -115,18 +116,44 @@ def print_info(cid):
     vid = df.loc[cid]
     print(vid['title'] + '\n' + vid['caption'])
 
-cid = test_keys[0]
+
+# Output file to results.json
+def run_test(matrix):
+    test_keys = [14434107, 249393804, 71964690, 78106175,
+                 228236677, 11374425, 93951774, 35616659,
+                 112360862, 116368488]
+
+    rec = pd.DataFrame()
+    # label = [test_keys[0]] * 1
+    for k in test_keys:
+        recs, _ = recommend(k, matrix)
+        for r in recs:
+            #         print(k)
+            l = df.loc[r]
+            l['recommend'] = k
+            #         l.set_index(k)
+            rec = rec.append(l)
+
+    rec['recommend'] = rec['recommend'].astype(int)
+    results = rec.set_index([rec['recommend'], rec.index]).to_json('results.json', orient='index')
+
+
+
+
+cid=14434107
+
 clip_categories['cat_name'] = clip_categories['cat'].map(lambda x: get_cat_name(x))
 # get_cat_name(clip_categories['cat'].iloc[0])
 df = clips.copy()
 df = preprocess(df)
 for idx, row in df.iterrows():
     row['words'] = remove_stop_words(row['words'])
+
 df, word_vector = get_word_vector(df)
 
 M = cosine_similarity(word_vector, word_vector)
 
-recs,scores = recommend(cid, df, M)
+recs, _ = recommend(cid, df, M)
 print(recs)
 rec = get_clip(cid, df)
 for r in recs:
